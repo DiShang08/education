a0445ce347c844e90904b88a19e1c1b30977c11d
DiShang08 <zethold@yandex.ru>
2022-06-21 14:10:03 +0300

    commit

--- codewars_examples/8kyu/examp_1.py
+++ codewars_examples/8kyu/examp_1.py
@@ -23,3 +23,12 @@
 
 # n = int(input())
 # print(count_sheep(n))
+
+
+# # 3
+# def better_than_average(class_points, your_points):
+#     return your_points > (sum(class_points)/len(class_points))
+
+
+# print(better_than_average([12, 23, 34, 45, 56, 67, 78, 89, 90], 69))
+# print(better_than_average([41, 75, 72, 56, 80, 82, 81, 33], 50))
