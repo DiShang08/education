59048fbb2e8b56f3a173125d0925430e7f0c2b2c
DiShang08 <107791828+DiShang08@users.noreply.github.com>
2022-07-18 21:06:51 +0300

    Delete file_3.py

--- file_3.py
+++
@@ -1,9 +0,0 @@
-file = open('numbers.txt', 'r', encoding='utf-8')
-summa = 0
-count = 0
-for line in file:
-    if len(line.strip()) == 3:
-        count += 1
-    elif len(line.strip()) == 2:
-        summa += int(line.strip())
-print(count, summa)
